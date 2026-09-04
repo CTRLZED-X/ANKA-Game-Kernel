7a0fc54813dbc48ac7550cd0479019d09f691403
zakaria ammar <sam678779@gmail.com>
2026-09-04 22:53:19 +0100

    feat: expose line-of-sight mechanics

---
+++ src/anka_game_kernel/mechanics/los/__init__.py
@@ -0,0 +1,6 @@
+"""Deterministic line-of-sight mechanics."""
+
+from anka_game_kernel.mechanics.los.service import LineOfSightService
+from anka_game_kernel.results.los import LineOfSightResult
+
+__all__ = ["LineOfSightResult", "LineOfSightService"]
